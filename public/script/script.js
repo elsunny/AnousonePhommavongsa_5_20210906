29ec3e6bbf553d57eb8fc400b6a74de70ec61f7f
anousone <anouph@yahoo.fr>
2021-09-13 09:06:02 +0200

    wip to dynamique

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,17 @@
 //récupération des données sur le serveur
-fetch("http://localhost:3000/api/cameras")
-    .then (data => data.json())
-    .then (jsonCameraItems => {
-        console.log(jsonCameraItems);
-    });
+// fetch("http://localhost:3000/api/cameras")
+//     .then (data => data.json())
+//     .then (jsonCameraItems => {
+//         console.log(jsonCameraItems);
+//         console.log(jsonCameraItems[1].name);
+//     });
+
+const productTitle = document.querySelector('.products__card__item__title');
+const productImage = document.querySelector('.products__card__item__image');
+console.log(productImage);
+
+fetch('http://localhost:3000/api/cameras')
+    .then(res => res.json())
+    .then(data => productTitle.textContent = data[1].name)
+    .then(data => console.log(data[1].name))
+     
