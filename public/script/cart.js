a46a10f07574eaaea07392126ae39cb6d71087a2
anousone <anouph@yahoo.fr>
2021-09-20 13:42:16 +0200

    wip cart working

--- public/script/cart.js
+++ public/script/cart.js
@@ -1,43 +1,42 @@
 // ///////////// js script for cart page cart.html //////////////////
+import { getProductData } from "./module.js";
+
 
 // get the informations from localstorage
 const cartProducts = JSON.parse(localStorage.getItem ('productInCart'));
 // get in variable the html content for the cart layout to be displayed
 const cartTemplate = document.querySelector('#cartTemplate');
 // get in a variable the template insertion point
-const cartInsert = document.querySelector('.cart'); 
+const cartInsert = document.querySelector('.carts'); 
 
 
-// find the id from the object
-const giveId = (index, arr) => {
-    return arr[index].id;
-}
 
-// get the informations relative to the id on the server
-async function getProductData(idProduct) {
-    // get the informations and transform the json data to javascript data
-    const reponse = await fetch(
-        `http://localhost:3000/api/cameras/${idProduct}`
-    );
-    const data = await reponse.json();
-    return data;
-}
+
+// // get the informations relative to the id on the server
+// async function getProductData(idProduct) {
+//     // get the informations and transform the json data to javascript data
+//     const reponse = await fetch(
+//         `http://localhost:3000/api/cameras/${idProduct}`
+//     );
+//     const data = await reponse.json();
+//     return data;
+// }
 // replace the template value by the server value
 // dynamique display of card for each product
-for (let i in cartProducts) {
-    const theId = cartProducts[i].id;
-    const product = getProductData(theId);
+cartProducts.forEach(async (cartProduct) => {
+    const theId = cartProduct.id;
+    const product = await getProductData(theId);
     console.log(product);
-    const cartProduct = cartTemplate.content
-        .querySelector(".cart")
+    const cart = cartTemplate.content
+        .querySelector(".product")
         .cloneNode(true);
-    cartProduct.querySelector(".product__caption h2").innerText = product.name;
-    cartProduct.querySelector(".products__card__price").innerText = product.price;
-    cartProduct.querySelector(".product img").src = product.imageUrl;
+    cart.querySelector(".product__caption h2").innerText = product.name;
+    cart.querySelector(".products__card__price").innerText = product.price;
+    cart.querySelector(".product img").src = product.imageUrl;
 
     // insert the code in the container 
-    cartInsert.appendChild(cartProduct);
-}
+    cartInsert.appendChild(cart);
+})
 
 
 
---
+++ public/script/module.js
@@ -0,0 +1,9 @@
+// get the informations relative to the id on the server
+export async function getProductData(idProduct) {
+    // get the informations and transform the json data to javascript data
+    const reponse = await fetch(
+        `http://localhost:3000/api/cameras/${idProduct}`
+    );
+    const data = await reponse.json();
+    return data;
+}
