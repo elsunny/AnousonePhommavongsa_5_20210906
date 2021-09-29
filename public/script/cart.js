e035dd489fa0794d311a6bbbaf3ba11fb29baf01
anousone <anouph@yahoo.fr>
2021-09-29 09:58:28 +0200

    wip cart post send

--- public/script/cart.js
+++ public/script/cart.js
@@ -1,18 +1,21 @@
 // ///////////// js script for cart page cart.html //////////////////
+
+// fetch request for a specific product id
 import { getProductData } from "./module.js";
 
 // get the informations from localstorage
 const cartProducts = JSON.parse(localStorage.getItem("productInCart"));
-// get the html content for the cart layout to be displayed
+
+// get the node for html template to display the cart content layout
 const cartTemplate = document.querySelector("#cartTemplate");
-// get the template insertion point
+
+// get the node for html insertion point
 const cartInsert = document.querySelector(".carts");
+
 // subtotal price
 let subtotalPrice = 0;
-// get the quantity insertion node
-const cartQuantity = document.querySelector(".product__quantity__number");
 
-// show message if cart is empty otherwise show the cart
+// ///////////////// show message if cart is empty otherwise show the cart /////////////
 if (cartProducts == null) {
     document.querySelector(".cart__message").innerText =
         "votre panier est vide";
@@ -30,6 +33,7 @@ if (cartProducts == null) {
             cartProduct.option;
         cart.querySelector(".products__card__price").innerText = product.price;
         cart.querySelector(".product img").src = product.imageUrl;
+        cart.querySelector("select[name=qty]").value = cartProduct.qty;
 
         // insert the code in the container
         cartInsert.appendChild(cart);
@@ -46,16 +50,76 @@ if (cartProducts == null) {
     });
 }
 
-// remove a product
+// ////////////// change the quantity //////////////
 
-// calculate the sum of cart products price
+// //////////// remove a product //////////////////
 
-// display the price
+// /////////////// send to server form and cart order //////////////////////
 
-// get the number of cart item
+// collect form data in a new object "order"
+// firstName, lastName, address, city, email
 
-// products array is grouped by id
+const form = document.getElementById("cartForm");
 
-//increase decrease quantity
-//remove article
-//send to server form and cart order
+class Customer {
+    constructor(firstName, lastName, address, city, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+    }
+}
+
+//product id from the customer cart
+const productId = cartProducts.map(p => {
+    return p.id;
+})
+
+// sending the order to server
+form.querySelector("button[name=submit]").addEventListener("click", (e) => {
+    e.preventDefault;
+
+    // get the form informations filled by the customer
+    let customer = new Customer(
+        form.querySelector("input[name=nom]").value,
+        form.querySelector("input[name=prenom]").value,
+        form.querySelector("input[name=adresse]").value,
+        form.querySelector("input[name=ville]").value,
+        form.querySelector("input[name=email]").value
+    );
+    
+    // collect customer informations and products ordered
+    let orderData = {
+        contact: customer,
+        products: productId
+    }
+
+    // option for the fectch with post method
+    const init = {
+        method: "post",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
+    };
+
+    // sending the datas to the server
+async function sendOrder(order) {
+    let response = await JSON.parse(fetch("http://localhost:3000/api/cameras/order", init))
+    console.log(response);
+}
+
+
+
+
+
+
+//     fetch("http://localhost:3000/api/cameras/order", init)
+//         // Converting to JSON
+//         .then((response) => response.json())
+
+//         // Displaying results to console
+//         .then((json) => console.log(json));
+});
