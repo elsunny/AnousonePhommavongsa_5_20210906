789795f2fbc7b25866a27675f3d6a0eac17b87fa
anousone <anouph@yahoo.fr>
2021-10-15 14:56:07 +0200

    wip

--- public/script/confirmation.js
+++ public/script/confirmation.js
@@ -12,7 +12,7 @@ try {
 
     // order confirmation message
     const message = `Nous vous remercions ${orderConfirmation.lastName} ${orderConfirmation.name} pour votre commande enregistrée sous le N°${orderConfirmation.orderID} et d'un montant total de ${totalAmount}`;
-    document.querySelector(".hero__title h1").textContent = message;
+    document.querySelector(".hero__title p").textContent = message;
 } catch (error) {
     console.log("Nous avons rencontré une erreur " + error);
     toastMessage("nous avons rencontré un problème!", "red", 5000);
